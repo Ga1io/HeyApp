da5a79be5255fb41a842c9132ab8e71062ba4dd8
tyg04308 <tyg04308@naver.com>
2020-10-02 19:31:27 +0900

    addresss, rate in Store

--- app/src/main/java/com/galio/heydrink/Data/Store.java
+++ app/src/main/java/com/galio/heydrink/Data/Store.java
@@ -7,6 +7,8 @@ public class Store implements Serializable {
     public String name;
     public double lang;
     public double lng;
+    public double rate;
+    public String address;
 
     public Store(){
 
